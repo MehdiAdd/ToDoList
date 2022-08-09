c942ea5057ab4c73c82c41bb51fffe3145eaac28
mehdiAD <mehdi.adnani2@gmail.com>
2022-08-09 19:52:31 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import {Task} from './Task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  atask:string="";
+  listTasks: Task[]=[];
+
+  ngOnInit():void {
+    this.listTasks=[];
+    this.atask="";
+    console.log('initttt')
+  }
+
+  addTask(){
+
+    if(this.atask !== ""){
+      let item: Task={
+        id: Date.now(),
+        task: this.atask
+      };
+      this.listTasks.push(item);
+      this.atask="";
+    }else{
+      alert("Enter a task");
+    }
+
+  }
+
+  deleteTask(id:number){
+    this.listTasks=this.listTasks.filter((item)=>item.id!==id);
+  }
+
+  clearAll(){
+    this.listTasks=[];
+    console.log('clear');
+  }
+
+
+
 }
